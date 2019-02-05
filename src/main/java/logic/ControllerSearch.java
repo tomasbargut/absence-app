b0ed9a98d7cb3f5ef25a5b1ea256305873193db6
Alejandro M Pirchi <alepirchi@live.com>
2019-02-05 20:58:41 -0300

    Agregadas nuevas clases de Search

---
+++ src/main/java/logic/ControllerSearch.java
@@ -0,0 +1,16 @@
+package logic;
+
+import data.DataUser;
+import entities.User;
+import entities.Service;
+import data.DataService;
+
+public class ControllerSearch {
+
+    private double[] priceRange;
+    private int locationRangeinKm;
+    private String[] keywords;
+
+    
+
+}
